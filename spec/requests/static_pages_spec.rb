d5544bad5800e21266976ced7e953442521a9df4
Kris Lapenskie <lapenskie@gmail.com>
2013-05-30 13:16:43 -0400

    Created mostly static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	
   describe "Home page" do
   	it "should have the content 'Stample App'" do
   		visit '/static_pages/home'
   		page.should have_content ('Sample App')
+    end
+    it "should have the title 'Home'" do
+    	visit '/static_pages/home'
+    	page.should have_selector('title',
+    		:text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -14,6 +20,11 @@ describe "Static pages" do
   		visit '/static_pages/help'
   		page.should have_content ('Help')
   	end
+  	it "should have the title 'Help'" do
+    	visit '/static_pages/help'
+    	page.should have_selector('title',
+    		:text => "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "About Page" do
@@ -22,5 +33,10 @@ describe "Static pages" do
   		visit '/static_pages/about'
   		page.should have_content ('About Us')
   	end
+  	it "should have the title 'About Us'" do
+    	visit '/static_pages/about'
+    	page.should have_selector('title',
+    		:text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
   end
 end
